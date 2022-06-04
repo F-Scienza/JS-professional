eb8f4647586f107c95580bfe93163b361f014a23
F-Scienza <cufa1905@gmail.com>
2022-06-04 17:53:29 -0300

    typescript

---
+++ Typescript/index.ts
@@ -0,0 +1,10 @@
+/*
+    Typescript es un lenguaje tipado
+        ayuda a prevenir errores de manera temprana
+
+        hay que instalar PARCEL-BUNDLER para que transpile 
+        (en lugar de usar liveserver usamo esto)
+
+        modificamos el package.json
+        
+*/
